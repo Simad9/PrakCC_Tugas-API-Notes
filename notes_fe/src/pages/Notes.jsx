42cc3054804693cee2a38243784c04c1b8d0cbe9
Wijdan <wijdanbita@gmail.com>
2025-03-18 08:15:17 +0700

    setting tugas 5 - update lagi

--- notes_fe/src/pages/Notes.jsx
+++ notes_fe/src/pages/Notes.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { BASE_URL } from "../utils";
+import { BASE_URL } from "../utils/utils.js";
 
 function NotesApp() {
   const [notes, setNotes] = useState([]);
